30be0c673afd0fa9308de9810306f106605ed87e
anvarinho <anvarinho@gmail.com>
2020-12-28 10:26:16 +0600

    Initial Commit

---
+++ Toguz Korgool/Toguz_KorgoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toguz_KorgoolApp.swift
+//  Toguz Korgool
+//
+//  Created by Anvar Jumabaev on 28.12.20.
+//
+
+import SwiftUI
+
+@main
+struct Toguz_KorgoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
